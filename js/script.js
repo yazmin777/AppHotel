85c005cecdc0f0322b07bf8df4f19f890d8acdc4
Leticia Tapia <leticia.tapia040208@gmail.com>
2017-10-14 12:00:31 -0500

    Actualizado alerta

--- js/script.js
+++ js/script.js
@@ -1,67 +1,69 @@
-var fn = {
+var fn={
+	init:function(){
+		$("#btnRegistrar").click(fn.registrarUsuario);
+		
+	},
+
 	deviceready:function(){
 		document.addEventListener("deviceready",fn.init,false);
 	},
-	init:function(){
-		$("#btnRegistrar").click(fn.registrarUsuario);
-	},
 	registrarUsuario:function(){
-		var nombre=$("#registro .nombre").val();
-
-		var email=$("#registro .email").val();
-		var password=$("#registro .password").val();
-		//console.log(nombre);
-		//console.log(email);
-		//console.log(password);
-
+		//selecciona un elemento de la pantalla de registro
+		var nombre   = $("#registro .nombre").val();
+		var email    = $("#registro .email").val();
+		var password = $("#registro .password").val();
+		/*console.log(nombre);
+		console.log(email);
+		console.log(password);*/
 		try{
-				if(email==""){
-					throw new Error("El email está vacio");
-				}
+			if(nombre == ""){
+				throw new Error("El nombre esta vacio");
+			}
+			if(email == ""){
+				throw new Error("El email esta vacio");
+			}
+			if(password == ""){
+				throw new Error("La contraseña esta vacia");
+			}
+			fn.nuevoUsuario(nombre, email, password);
 
-				if(nombre==""){
-					throw new Error("El nombre está vacio");
-				}
+			$("#registro .nombre").val("");
+			$("#registro .email").val("");
+			$("#registro .password").val("");
 
-				if(password==""){
-					throw new Error("El password está vacio");
-				}
-
-				fn.nuevoUsuario(nombre,email,password);
-
-
+			window.location.href = "#inicio";
 		}
 		catch(error){
 			alert(error);
 		}
+
 	},
-	nuevoUsuario:function(nombre,email,password){
-		var usuario={};
-		usuario.nombre=nombre;
-		usuario.email=email;
-		usuario.password=password;
-		var usuariocadena=JSON.stringify(usuario);
-		//console.log(usuariocadena);
-		//GUARDAR EN LOCALSTORAGE
-		window.localStorage.setItem("usuario",usuariocadena);
-		var obtenerusuario= window.localStorage.getItem("usuario");
-		var objusuario = JSON.parse(obtenerusuario);
-		console.log(objusuario.nombre);
+	nuevoUsuario: function(nombre, email, password){
+		var usuario      = {};
+		usuario.nombre   = nombre;
+		usuario.email    = email;
+		usuario.password = password;
 
-		alert("Usuario: "+objusuario.nombre+" guardado");
-		$("#registro .nombre").val("");
-		$("#registro .email").val("");
-		$("#registro .password").val("");
+		var usuarioCadena = JSON.stringify(usuario);
+		//console.log(usuarioCadena);
 
+		//GURDAR EN LOCALSTORAGE
+		window.localStorage.setItem("usuario", usuarioCadena);
+
+		alert("Usuario:" +usuario.nombre+" guardado");
+
+		
+		var obtenerNombre = window.localStorage.getItem("usuario");
+		var NombreR  = JSON.parse(obtenerNombre);
+		console.log(NombreR.nombre);
+		$("#msj").html ("Bienvenido " + nombre);
 
 
 	}
-
 };
 
-
-//para compilar
+//COMPILAR PARA CELULAR
 fn.deviceready();
 
-//para pruebas en navegador
+//PRUEBAS EN NAVEGADOR
 //fn.init();
