333b2f14ae6431b6859c6be8e9857737464d8d28
Yazmin <yazmin777@gmail.com>
2017-11-11 10:12:53 -0600

    geo

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,7 +2,7 @@ var geo= {
 	exito:function(position){
 		var coordenadas={
 			lat:19.060354,
-			long: -98.221015
+			lng: -98.221015
 		};
 
 		var map = new google.maps.Map(document.getElementById('mapa'), {
