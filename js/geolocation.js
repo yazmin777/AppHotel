934706f9989d43f11ac29132e73d08e6e751c647
Yazmin <yazmin777@gmail.com>
2017-11-11 11:45:52 -0600

    geo

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,21 +1,37 @@
 var geo= {
 	exito:function(position){
 		//alert("exito");
-		var coordenadas={
+		var coordenadasHotel={
 			lat:19.060354,
 			lng: -98.221015
 		};
         
+        var coordenadasUsuario{
+        	lat: position.coords.latitude,
+        	lng: position.coords.longitude
+        }
 		var map = new google.maps.Map(document.getElementById('mapa'), {
-          zoom: 15,
-          center: coordenadas
+          zoom: 10,
+          center: coordenadasUsuario
         });
 
-        var marker = new google.maps.Marker({
-          	position: coordenadas,
-          	map: map
+        var directionsDisplay=new google.maps.DirectionsRenderer({
+        	map:map
         });
-		
+
+        var request={
+        	destination: coordenadasHotel,
+        	origin: coordenadasUsuario,
+        	travelMode: google.maps.TravelMode.DRIVING
+        };
+
+        var directionsService= new google.maps.DirectionsService();
+
+        directionsService.route(request,function(response,status){
+        	if (status==google.maps.DirectionsStatus.OK){
+        		directionsDisplay.setDirections(response);
+        	}
+        });
 	},
 
 	error:function(error){
@@ -23,7 +39,6 @@ var geo= {
 	},
 
 	obtenerPosicion:function(){
-
        navigator.geolocation.getCurrentPosition(geo.exito,geo.error);
 	}
 };
