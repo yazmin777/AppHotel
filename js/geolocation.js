2ebd424f33153b3bf262fab1ab6a5a7014d0c645
Yazmin <yazmin777@gmail.com>
2017-12-02 10:42:45 -0600

    agregando base de datos

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,45 +1,44 @@
-var geo= {
-	exito:function(position){
-		//alert("exito");
-		var coordenadasHotel={
-			lat:19.060354,
-			lng: -98.221015
-		};
-        
-        var coordenadasUsuario={
-        	lat: position.coords.latitude,
-        	lng: position.coords.longitude
-        }
-		var map = new google.maps.Map(document.getElementById('mapa'), {
-          zoom: 10,
-          center: coordenadasUsuario
-        });
-
-        var directionsDisplay=new google.maps.DirectionsRenderer({
-        	map:map
-        });
-
-        var request={
-        	destination: coordenadasHotel,
-        	origin: coordenadasUsuario,
-        	travelMode: google.maps.TravelMode.DRIVING
+var geo = {
+    exito: function(position){
+        var coordenadasHotel = {
+            lat: 19.054802,
+            lng: -98.225602
         };
 
-        var directionsService= new google.maps.DirectionsService();
+        var coordenadasUsuario = {
+            lat:  position.coords.latitude,
+            lng:  position.coords.longitude
+        };
 
-        directionsService.route(request,function(response,status){
-        	if (status==google.maps.DirectionsStatus.OK){
-        		directionsDisplay.setDirections(response);
-        	}
+        var map = new google.maps.Map(document.getElementById('mapa'), {
+            zoom: 14,
+            center: coordenadasUsuario
         });
-	},
 
-	error:function(error){
-		alert("ERROR:"+error.message);
-	},
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        });
 
-	obtenerPosicion:function(){
-		alert("hola");
-       navigator.geolocation.getCurrentPosition(geo.exito,geo.error);
-	}
+        var request = {
+            destination: coordenadasHotel,
+            origin: coordenadasUsuario,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+
+          // Pass the directions request to the directions service.
+        var directionsService = new google.maps.DirectionsService();
+        
+        directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              // Display the route on the map.
+              directionsDisplay.setDirections(response);
+            }
+        });
+    },
+    error: function(error){
+        alert("ERROR: "+ error.message);
+    },
+    obtenerPosicion: function(){
+        navigator.geolocation.getCurrentPosition(geo.exito, geo.error);
+    }
 };
